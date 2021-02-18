116769be6bde9d1ad2fb8040fc3aed66dae94730
galaxy4276 <galaxyhi4276@gmail.com>
2021-02-19 07:47:27 +0900

    feat: useForm errors 확인

--- react/src/react-hook-form/Basic.js
+++ react/src/react-hook-form/Basic.js
@@ -7,19 +7,24 @@ const Basic = () => {
   const { register, errors, handleSubmit } = useForm();
   const onSubmit = data => alert(JSON.stringify(data));
 
+  // errors 내용 확인
+  React.useEffect(() => {
+    console.log(errors);
+  }, [errors]);
+
   return (
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
 
         <div>
           <label htmlFor="firstName">First Name</label>
-          <input name="firstName" placeholder="성을 입력하세요." ref={register({ required: true })} />
+          <input name="firstName" placeholder="성을 입력하세요." ref={register({ required: 'required firstName' })} />
           {errors.firstName && <p style={{ color: 'red' }}>성을 필수로 입력해주세요.</p>}
         </div>
 
         <div>
           <label htmlFor="lastName">Last Name</label>
-          <input name="lastName" placeholder="이름을 입력하세요." ref={register({ required: true })} />
+          <input name="lastName" placeholder="이름을 입력하세요." ref={register({ required: 'required lastName' })} />
           {errors.lastName && <p style={{ color: 'red' }}>이름을 필수로 입력해주세요.</p>}
         </div>
 
@@ -29,7 +34,7 @@ const Basic = () => {
             type="checkbox"
             name="isHuman"
             value="yes"
-            ref={register({ required: true })}
+            ref={register({ required: 'required check' })}
           />
           {errors.isHuman && <p style={{ color: 'red' }}>반드시 체크해주세요.</p>}
         </div>
@@ -40,7 +45,7 @@ const Basic = () => {
             name="email"
             placeholder="이메일을 입력하세요."
             type="email"
-            ref={register({ required: true })}
+            ref={register({ required: 'required Email' })}
           />
           {errors.email && <p style={{ color: 'red' }}>이메일을 반드시 입력해주세요.</p>}
         </div>
