9710faa32894788bfdcc42a346fce2c82d32640e
최은기 <galaxyhi4276@gmail.com>
2021-05-03 05:58:57 +0900

    laravel migration

---
+++ toravel/database/seeders/TodoTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TodoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('todos')->insert([
+            'content' => '내가 해야 할 일',
+        ]);
+    }
+}
